c5d6c80dee50021f8fcb4c0143ae248a580e70f2
Jim Brown <jim.brown@parivedasolutions.com>
2020-01-05 21:02:59 -0600

    Updates

---
+++ precache-manifest.0d9b0ef2ba9f3a21abc514eda068ec24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1184886a1712ac1dc7580212e7196d7",
+    "url": "/action-items/index.html"
+  },
+  {
+    "revision": "862391ad944dcef11618",
+    "url": "/action-items/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "586bd8ceafe4fd8d8baf",
+    "url": "/action-items/static/js/2.1c92a7e0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/action-items/static/js/2.1c92a7e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "862391ad944dcef11618",
+    "url": "/action-items/static/js/main.9151f1f3.chunk.js"
+  },
+  {
+    "revision": "3debe59d55686b919e29",
+    "url": "/action-items/static/js/runtime-main.174313e9.js"
+  }
+]);
